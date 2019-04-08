3672447edd26545a1aac8ba97b4c511bf41daee7
Lucas Martins <lucaslealm@gmail.com>
2019-04-08 17:08:33 -0300

    updated controller with create/move rover

--- app/controllers/rovers_controller.rb
+++ app/controllers/rovers_controller.rb
@@ -6,4 +6,25 @@ class RoversController
   def initialize
     @view = RoverView.new
   end
+
+  def create_rover
+    latitude = @view.ask("What's the current latitude of the rover on the plateau? (Choose from 0 to 5)")
+    longitude = @view.ask("What's the current longitude of the rover on the plateau? (Choose from 0 to 5)")
+    direction = @view.ask("What's the current direction of the rover on the plateau? (Type N for North, S for South, E for East and W for West)")
+    @new_rover = Rover.new(latitude: latitude,
+                           longitude: longitude,
+                           direction: direction)
+  end
+
+  def move_command
+
+  end
+
+
+  def move
+    #usar each_char!
+  end
+
+
+
 end
