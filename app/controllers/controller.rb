9c9b8b5b34cb90becc0427b259152a46eeedde97
Lucas Martins <lucaslealm@gmail.com>
2019-04-09 21:31:02 -0300

    rover final position implemented

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -11,34 +11,36 @@ class Controller
   def create_plateau
     x_axis = @plateau_view.ask("What's the horizontal size (X axis) of the plateau?")
     y_axis = @plateau_view.ask("What's the vertical size (Y axis) of the plateau?")
-    @new_plateau = Plateau.new(x_axis: x_axis,
-                               y_axis: y_axis)
+    @plateau = Plateau.new(x_axis: x_axis,
+                           y_axis: y_axis)
   end
 
   def create_rover
-    if @new_plateau.nil?
-      puts "Please, define a plateau before deploying a new rover"
+    if @plateau.nil?
+      puts "Please, define a plateau before deploying a new rover."
     else
       longitude = @rover_view.ask("What's the current longitude (X axis) of the rover on the plateau?")
       latitude = @rover_view.ask("What's the current latitude (Y axis) of the rover on the plateau?")
       direction = @rover_view.ask("What's the current direction of the rover on the plateau? (Type N for North, S for South, E for East and W for West)")
-      @new_rover = Rover.new(latitude: latitude,
-                             longitude: longitude,
-                             direction: direction)
+      @rover = Rover.new(latitude: latitude,
+                         longitude: longitude,
+                         direction: direction)
     end
   end
 
   def new_coordinates
-    if @new_rover.nil?
-      puts "Please, deploy a new rover before sending commands"
+    if @rover.nil?
+      puts "Please, deploy a new rover before sending commands."
     else
       command_from_user
       print `clear`
       check_command
+      p @command
       @command.each_char do |letter|
         moving_condition(letter)
       end
     end
+    final_position
   end
 
   private
@@ -53,41 +55,50 @@ class Controller
   end
 
   def moving_condition(letter)
-    spin_left if letter.capitalize == 'L'
-    spin_right if letter.capitalize == 'R'
-    move if letter.capitalize == 'M'
+    if letter.capitalize == 'L'
+      spin_left
+    elsif letter.capitalize == 'R'
+      spin_right
+    elsif letter.capitalize == 'M'
+      move
+    end
   end
 
   def spin_left
-    case @new_rover.direction
-    when 'N' then @new_rover.direction = 'W'
-    when 'W' then @new_rover.direction = 'S'
-    when 'S' then @new_rover.direction = 'E'
-    when 'E' then @new_rover.direction = 'N'
+    case @rover.direction
+    when 'N' then @rover.direction = 'W'
+    when 'W' then @rover.direction = 'S'
+    when 'S' then @rover.direction = 'E'
+    when 'E' then @rover.direction = 'N'
     end
   end
 
   def spin_right
-    case @new_rover.direction
-    when 'N' then @new_rover.direction = 'E'
-    when 'W' then @new_rover.direction = 'N'
-    when 'S' then @new_rover.direction = 'W'
-    when 'E' then @new_rover.direction = 'S'
+    case @rover.direction
+    when 'N' then @rover.direction = 'E'
+    when 'W' then @rover.direction = 'N'
+    when 'S' then @rover.direction = 'W'
+    when 'E' then @rover.direction = 'S'
     end
   end
 
   def move
-    if @new_rover.direction == 'N'
-      @new_rover.latitude = @new_rover.latitude += 1
-    elsif @new_rover.direction == 'E'
-      @new_rover.longitude = @new_rover.longitude += 1
-    elsif @new_rover.direction == 'S'
-      @new_rover.latitude = @new_rover.latitude -= 1
-    elsif @new_rover.direction == 'W'
-      @new_rover.longitude = @new_rover.longitude -= 1
+    if @rover.direction == 'N'
+      @rover.latitude = @rover.latitude += 1
+    elsif @rover.direction == 'E'
+      @rover.longitude = @rover.longitude += 1
+    elsif @rover.direction == 'S'
+      @rover.latitude = @rover.latitude -= 1
+    elsif @rover.direction == 'W'
+      @rover.longitude = @rover.longitude -= 1
     end
   end
 
   def final_position
+    if @rover.latitude.between?(0, @plateau.y_axis) && @rover.longitude.between?(0, @plateau.x_axis)
+      puts "The rover's current position is: #{@rover.longitude} #{@rover.latitude} #{@rover.direction}"
+    else
+      puts "The rover fell from the plateau. Try deploying another one!"
+    end
   end
 end
